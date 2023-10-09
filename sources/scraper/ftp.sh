b18f6187ce7d5096be665d2babf857126531bfcc
nich0las <63231742+Nicholas-wei@users.noreply.github.com>
2023-10-09 17:03:26 +0800

    Update ftp.sh

--- sources/scraper/ftp.sh
+++ sources/scraper/ftp.sh
@@ -5,12 +5,12 @@ wget --mirror -A zip,pkg,rar,bin,img --ignore-case ftp://ftp2.dlink.com/PRODUCTS
 # wget --mirror -A zip,pkg,rar,bin,img ftp://ftp.dlink.ru/pub -P output -P output
 # wget --mirror -A zip,pkg,rar,bin,img ftp://ftp.d-link.co.za -P output
 
-wget --mirror -A image --ignore-case ftp://ftp.avm.de/fritz.box -P output
+wget --mirror -A image --ignore-case ftp://ftp.avm.de/fritz.box -P output --no-check-certicicate
 
 wget --mirror -A img,chk,bin,stk,zip,tar,sys,rar,pkg,rmt --ignore-case ftp://downloads.netgear.com -P output
 
-wget --mirror -A zip --ignore-case ftp://ftp.zyxel.com -P output
+wget --mirror -A zip --ignore-case ftp://ftp.zyxel.com -P output --no-check-certicicate
 
-wget --mirror -A bin --ignore-case ftp://ftp.dd-wrt.com -P output
+wget --mirror -A bin --ignore-case ftp://ftp.dd-wrt.com -P output --no-check-certicicate
 
 git clone https://git.kernel.org/pub/scm/linux/kernel/git/firmware/linux-firmware.git output/linux-firmware
